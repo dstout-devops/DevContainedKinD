3e6192e54c1c494e85e6eca8c650c294b5197eb7
dstout-devops <dstout.devops@gmail.com>
2024-10-18 16:50:52 +0000

    add ansible

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Completed initial container build..."
 
-# install KinD
-go install sigs.k8s.io/kind@latest
+# install k3s
+#curl -Lo /usr/local/bin/k3s https://github.com/k3s-io/k3s/releases/download/v1.26.5+k3s1/k3s
+#chmod a+x /usr/local/bin/k3s
+activate-global-python-argcomplete --user
