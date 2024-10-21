927df69384df11220e311b78a39cc69f054215b8
dstout-devops <dstout.devops@gmail.com>
2024-10-21 16:24:03 +0000

    add ansible and terraform

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "Completed initial container build..."
 
-# install k3s
-#curl -Lo /usr/local/bin/k3s https://github.com/k3s-io/k3s/releases/download/v1.26.5+k3s1/k3s
-#chmod a+x /usr/local/bin/k3s
-activate-global-python-argcomplete --user
+# create ansible execution environment
+ansible-builder build --file .devcontainer/ansible/execution-environment.yml --tag ansible_ee --container-runtime docker --context /var/tmp/context
